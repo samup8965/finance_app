7c5b2450805df0a44ab2c756355e873afd09d361
Samuel <samuparra.c@gmail.com>
2025-06-23 10:51:53 +0100

    Added basic sign in and sign out features

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  const { session, signOut } = UserAuth();
+  const navigate = useNavigate();
+
+  console.log(session);
+
+  const handleSignOut = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      await signOut();
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1> Welcome, {session?.user?.email}</h1>
+      <button onClick={handleSignOut}>Signout</button>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { UserAuth } from "../context/AuthContext.tsx";
 
 const Signin = () => {
-  return <div>signin</div>;
+  // States
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { session, signInUser } = UserAuth();
+  const navigate = useNavigate();
+  console.log(session);
+  console.log(email, password);
+
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = await signInUser(email, password);
+
+      if (result.success) {
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      setError("an error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <form onSubmit={handleSignIn} className="signup-form">
+        <h2 className="signup-heading">Sign In</h2>
+        <p className="signup-subtext">
+          Dont have an account?{" "}
+          <Link to="/signup" className="signin-link">
+            Sign Up!
+          </Link>
+        </p>
+        <div className="signup-input-group">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+            className="signup-input"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+            className="signup-input"
+          />
+          <button type="submit" className="signup-button">
+            Sign In
+          </button>
+          {error && <p className="signup-error">{error}</p>}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
