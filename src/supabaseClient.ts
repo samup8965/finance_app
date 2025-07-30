b7b8c84d844cad050f2d9b7ce3a09cc8b4498c57
Samuel <samuparra.c@gmail.com>
2025-07-30 16:22:02 +0100

    Fixed session persistence issue in production by storing it in localStorage instead of session

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -7,7 +7,7 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
-    storage: window.sessionStorage,
+    storage: window.localStorage,
     autoRefreshToken: true,
     detectSessionInUrl: true,
   },
